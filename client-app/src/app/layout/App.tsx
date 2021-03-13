0de7daa2bbe26f6253b259b1c7d52265307245cb
Veso <Ambroza8888@gmail.com>
2021-03-13 02:08:24 -0800

    Added 2 Components - ActivityDashboard and ActivityList

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,8 +1,9 @@
-import React, { Fragment, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Container, List } from 'semantic-ui-react';
 import { Activity } from '../models/activity';
-import NavBar from './navbar';
+import NavBar from './Navbar';
+import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
 
 function App() {
   const [activities, setActivities] = useState<Activity[]>([]);
@@ -20,11 +21,7 @@ function App() {
       <NavBar />
 
       <Container style={{marginTop: '7em'}}>
-        <List>
-          {activities.map((activity) => {
-            return <List.Item key={activity.id}>{activity.title}</List.Item>;
-          })}
-        </List>
+        <ActivityDashboard activities ={activities} />
       </Container>
     </>
   );
