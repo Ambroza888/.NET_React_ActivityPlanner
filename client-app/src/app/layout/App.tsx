ca2c7b30e742973c6d3fcb533f099a04aecef3f8
Veso <Ambroza8888@gmail.com>
2021-03-13 00:54:44 -0800

    Added navbar in React

---
+++ client-app/src/app/layout/App.tsx
@@ -0,0 +1,33 @@
+import React, { Fragment, useEffect, useState } from 'react';
+import axios from 'axios';
+import { Container, List } from 'semantic-ui-react';
+import { Activity } from '../models/activity';
+import NavBar from './navbar';
+
+function App() {
+  const [activities, setActivities] = useState<Activity[]>([]);
+
+  useEffect(() => {
+    axios
+      .get<Activity[]>('http://localhost:5000/api/activities')
+      .then((response) => {
+        setActivities(response.data);
+      });
+  }, []);
+
+  return (
+    <>
+      <NavBar />
+
+      <Container style={{marginTop: '7em'}}>
+        <List>
+          {activities.map((activity) => {
+            return <List.Item key={activity.id}>{activity.title}</List.Item>;
+          })}
+        </List>
+      </Container>
+    </>
+  );
+}
+
+export default App;
