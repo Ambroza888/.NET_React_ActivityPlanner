fb256ae45cd0ba68106beb45ca56cf14b5de142d
Veso <Ambroza8888@gmail.com>
2021-06-27 17:45:24 -0700

    rr

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -8,7 +8,7 @@ import { observer } from 'mobx-react-lite';
 
 function App() {
 
-  const {activityStore} = useStore();
+  const { activityStore } = useStore();
 
   useEffect(() => {
     activityStore.loadActivities();
