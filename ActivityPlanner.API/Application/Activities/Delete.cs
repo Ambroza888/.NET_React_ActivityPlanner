880279ad0e14030adbf5df6efaa5ad62db27c828
Veso <Ambroza8888@gmail.com>
2021-07-06 00:50:06 -0700

    Added middleware for Handling Exceptions.

--- ActivityPlanner.API/Application/Activities/Delete.cs
+++ ActivityPlanner.API/Application/Activities/Delete.cs
@@ -27,7 +27,7 @@ namespace Application.Activities
       {
         Activity activity = await _context.Activities.FindAsync(request.Id);
 
-        if (activity == null) return null;
+        // if (activity == null) return null;
 
         _context.Remove(activity);
 
