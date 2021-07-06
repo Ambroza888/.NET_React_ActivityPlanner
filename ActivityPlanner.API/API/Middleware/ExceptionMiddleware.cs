6b83f996a124425528e29a9c7331373d5db41879
Veso <Ambroza8888@gmail.com>
2021-07-06 00:54:33 -0700

    rr

--- ActivityPlanner.API/API/Middleware/ExceptionMiddleware.cs
+++ ActivityPlanner.API/API/Middleware/ExceptionMiddleware.cs
@@ -38,7 +38,6 @@ namespace API.Middleware
                         : new AppException(context.Response.StatusCode, "Server Error from Exception Middleware handler");
 
                     var options = new JsonSerializerOptions{PropertyNamingPolicy = JsonNamingPolicy.CamelCase};
-
                     var json = JsonSerializer.Serialize(response, options);
 
                     await context.Response.WriteAsync(json);
