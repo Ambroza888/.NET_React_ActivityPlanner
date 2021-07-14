ced16b86e8a2696edf40f3bac8bf1fa7c72e1a95
Veso <Ambroza8888@gmail.com>
2021-07-14 12:04:45 -0700

    added validation example

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -1,11 +1,12 @@
 import { observer } from 'mobx-react-lite';
 import React, { ChangeEvent, useEffect, useState } from 'react';
 import { Link, useHistory, useParams } from 'react-router-dom';
-import { Button, Segment } from 'semantic-ui-react';
+import { Button, FormField, Label, Segment } from 'semantic-ui-react';
 import LoadingComponent from '../../../app/layout/LoadingComponent';
 import { useStore } from '../../../app/stores/store';
 import { v4 as uuid } from 'uuid';
-import { Formik, Form, Field } from 'formik';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
 
 export default observer(function ActivityForm() {
   const { activityStore } = useStore();
@@ -29,6 +30,10 @@ export default observer(function ActivityForm() {
     city: '',
     venue: '',
   });
+
+  const validationSchema = Yup.object({
+    title: Yup.string().required('The activity title is required')
+  })
 
   useEffect(() => {
     if (id) loadActivity(id).then((activity) => setActivity(activity!));
@@ -61,13 +66,19 @@ export default observer(function ActivityForm() {
 
   return (
     <Segment clearing>
-      <Formik enableReinitialize initialValues={activity} onSubmit={values => console.log(values)}>
+      <Formik
+        validationSchema={validationSchema}
+        enableReinitialize
+        initialValues={activity}
+        onSubmit={values => console.log(values)}>
         {({ handleSubmit }) => (
           <Form className='ui form' onSubmit={handleSubmit} autoComplete='off'>
-            <Field
-              placeholder='Title'
-              name='title'
-            />
+            <FormField>
+              <Field placeholder='Title' name='title'/>
+              <ErrorMessage name='title'
+                render={error => <Label basic color='red' content={error} />}
+              />
+            </FormField>
             <Field
               placeholder='Description'
               name='description'
