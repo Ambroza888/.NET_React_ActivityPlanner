4a7d34ca3ec60309ac16bc8b7b506f12efde15a8
Veso <Ambroza8888@gmail.com>
2021-07-26 17:14:09 -0700

    Added Attendance handler and rout for: 1) If you are host on activity to set it to true or false for cancelet
    2) if you are not the host but you would liek to join the activity
    
    3) and leave the activity if you are already in it.

--- ActivityPlanner.API/Application/Activities/List.cs
+++ ActivityPlanner.API/Application/Activities/List.cs
@@ -32,6 +32,7 @@ namespace Application.Activities
                         .Include(a => a.Attendees)
                         .ThenInclude(u => u.AppUser)
                         .ToListAsync();
+
                     var activitiesToReturn = _mapper.Map<List<ActivityDto>>(activities);
 
                         To make cleaner SQL call to the DB We are going to add ProjectTo method
--- ActivityPlanner.API/Application/Interfaces/IUserAccessor.cs
+++ ActivityPlanner.API/Application/Interfaces/IUserAccessor.cs
@@ -2,6 +2,7 @@ namespace Application.Interfaces
 {
     public interface IUserAccessor
     {
+        // describing class is in other solution Infrastructure - Security
         string GetUsername();
     }
 }
