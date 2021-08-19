93bbff2cfe31158c939bd914273ff54121029c42
Veso <Ambroza8888@gmail.com>
2021-08-18 19:00:14 -0700

    added PhotoWidget and Dropzone.PREVIEW

--- client-app/src/app/common/imageUpload/PhotoUploadWidget.tsx
+++ client-app/src/app/common/imageUpload/PhotoUploadWidget.tsx
@@ -1,21 +1,27 @@
-import React from 'react';
-import { Grid, Header } from 'semantic-ui-react';
+import React, { useState } from 'react';
+import { Grid, Header, Image } from 'semantic-ui-react';
 import PhotoWidgetDropzone from './PhotoWidgetDropzone';
 
 export default function PhotoUploadWidget() {
+  const [files, setFiles] = useState<any>([]);
+
   return (
     <Grid>
       <Grid.Column width='4'>
-        <Header sub color='teal' content='Step 1 - Add Photo'></Header>
-        <PhotoWidgetDropzone/>
+        <Header sub color='teal' content='Step 1 - Add Photo'/>
+        <PhotoWidgetDropzone setFiles={setFiles}/>
       </Grid.Column>
       <Grid.Column width='1'/>
       <Grid.Column width='4'>
-        <Header sub color='teal' content='Step 2 - Resize Image'></Header>
+        <Header sub color='teal' content='Step 2 - Resize Image'/>
+        { files &&
+          files.length > 0 &&
+          (<Image src={files[0].preview} />)
+        }
       </Grid.Column>
       <Grid.Column width='1'/>
       <Grid.Column width='4'>
-        <Header sub color='teal' content='Step 1 - Preview & Upload'></Header>
+        <Header sub color='teal' content='Step 1 - Preview & Upload' />
       </Grid.Column>
     </Grid>
   )
