ffc843183fc49f5c4e71a60d70fc7714a2dc859c
Veso <Ambroza8888@gmail.com>
2021-08-30 19:05:12 -0700

    Added Following class, Create command class (toggle).
    also Follwing Controller. Now able to see how many people are following and been followed.

--- ActivityPlanner.API/Application/Core/MappingProfiles.cs
+++ ActivityPlanner.API/Application/Core/MappingProfiles.cs
@@ -21,8 +21,9 @@ namespace Application.Core
                 .ForMember(d => d.Image, o => o.MapFrom(src => src.AppUser.Photos.FirstOrDefault(x => x.IsMain).Url));
             // we are using Profiles.Profile full path because automapper is using Profile class too.
             CreateMap<AppUser, Profiles.Profile>()
-                .ForMember(d => d.Image, o => o.MapFrom(src => src.Photos.FirstOrDefault(x => x.IsMain).Url));
-
+                .ForMember(d => d.Image, o => o.MapFrom(src => src.Photos.FirstOrDefault(x => x.IsMain).Url))
+                .ForMember(d => d.FollowersCount, o => o.MapFrom(s => s.Followers.Count))
+                .ForMember(d => d.FollowingCount, o => o.MapFrom(s => s.Followings.Count));
             CreateMap<Comment, CommentDto>()
                 .ForMember(d => d.Username, o => o.MapFrom(src => src.Author.UserName))
                 .ForMember(d => d.DisplayName, o => o.MapFrom(src => src.Author.DisplayName))
