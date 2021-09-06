d4bd49446f7d4769957e2eea4be2d0e6b853ff2d
Veso <Ambroza8888@gmail.com>
2021-09-05 19:10:32 -0700

    added pagination on front-end

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -2,6 +2,7 @@ import axios, { AxiosError, AxiosResponse } from 'axios';
 import { toast } from 'react-toastify';
 import { history } from '../..';
 import { Activity, ActivityFormValues } from '../models/activity';
+import { PaginatedResult } from '../models/pagination';
 import { Photo, Profile } from '../models/profile';
 import { User, UserFormValues } from '../models/user';
 import { store } from '../stores/store';
@@ -23,7 +24,14 @@ axios.defaults.baseURL = 'http://localhost:5000/api';
 
 axios.interceptors.response.use(async response => {
     await sleep(1000);
+
+    const pagination = response.headers['pagination'];
+    if (pagination) {
+      response.data = new PaginatedResult(response.data, JSON.parse(pagination));
+      return response as AxiosResponse<PaginatedResult<any>>;
+    }
     return response;
+
 }, (err: AxiosError) => {
   const {data, status, config} = err.response!;
   switch (status) {
@@ -69,7 +77,8 @@ const requests = {
 }
 
 const Activities = {
-  list: () => requests.get<Activity[]>('/activities'),
+  list: (params: URLSearchParams) => axios.get<PaginatedResult<Activity[]>>('/activities', {params: params})
+    .then(response => response.data),
   details: (id: string)=> requests.get<Activity>(`/activities/${id}`),
   create: (activity:ActivityFormValues) => requests.post<void>('/activities', activity),
   update: (activity:ActivityFormValues) => requests.put<void>(`/activities/${activity.id}`, activity),
--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -4,6 +4,7 @@ import { Activity, ActivityFormValues } from '../models/activity';
 import { format } from 'date-fns';
 import { store } from './store';
 import { Profile } from '../models/profile';
+import { Pagination, PagingParams } from '../models/pagination';
 
 export default class ActivityStore {
 
@@ -12,9 +13,22 @@ export default class ActivityStore {
   editMode = false;
   loading = false;
   loadingInitial = false;
+  pagination: Pagination | null = null;
+  pagingParams = new PagingParams();
 
   constructor() {
     makeAutoObservable(this);
+  }
+
+  setPagingParams = (pagingParams: PagingParams) => {
+    this.pagingParams = pagingParams;
+  }
+
+  get axiosParams() {
+    const params = new URLSearchParams();
+    params.append('pageNumber', this.pagingParams.pageNumber.toString());
+    params.append('pageSize', this.pagingParams.pageSize.toString());
+    return params;
   }
 
   get activitiesByDate() {
@@ -35,15 +49,20 @@ export default class ActivityStore {
   loadActivities = async () => {
     this.loadingInitial = true;
     try {
-      const activities = await agent.Activities.list();
-      activities.forEach(activity => {
+      const result = await agent.Activities.list(this.axiosParams);
+      result.data.forEach(activity => {
         this.setActivity(activity);
       });
+      this.setPagination(result.pagination);
       this.setLoadingInitial(false);
     } catch (error) {
       console.log(error);
       this.setLoadingInitial(false);
     }
+  }
+
+  setPagination = (pagination:Pagination) => {
+    this.pagination = pagination;
   }
 
   loadActivity = async (id:string) => {
